14f275c9a152cd08d62e15fef42656a8574540f8
Sirapat Phunjamaneechot <p.sirapatp@gmail.com>
2025-03-26 16:47:52 +0700

    Progress check

--- src/navbuttons.js
+++ src/navbuttons.js
@@ -8,12 +8,19 @@ function setNav(event, val) {
         let targetBtn = document.getElementById(`${val.toLowerCase()}Button`);
         targetBtn.className += " active";
     }
+
+    document.getElementById(val).style.display = "block";
 }
 
 function clearNav() {
     let navButtons = document.getElementsByClassName("navbtn");
     for (let b = 0; b < navButtons.length; b++) {
         navButtons[b].className = navButtons[b].className.replace(" active", "");
+    }
+
+    let tabs = document.getElementsByClassName("navtab");
+    for (let t = 0; t < tabs.length; t++) {
+        tabs[t].style.display = "none";
     }
 }
 
